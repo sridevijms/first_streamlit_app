dfd8cf17d132facfc9698789c0e802ce71a1c471
Sridevi <sridevijms@gmail.com>
2022-10-28 23:02:12 +0530

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit;
 streamlit.title("my Test page in streamlit")
+streamlit.header("my Test page in header")
+streamlit.subheader("my Test page in subheader")
+streamlit.text("my Test page in text1")
+streamlit.text("my Test page in text2")
