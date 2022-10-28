4435a27b624c7d74aef0756010537f4743a8a2d6
Sridevi <sridevijms@gmail.com>
2022-10-28 23:04:27 +0530

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,4 @@ streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑Hard-Boiled Free-Range Egg')
 streamlit.text('   🍞Hard-Boiled Free-Range Egg')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
