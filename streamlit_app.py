ca6472bf05400714f3ba979b3f20db1fc447df40
Sridevi Murugayen <sridevi.murugayen@fmr.com>
2022-11-01 12:35:45 +0530

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
 
