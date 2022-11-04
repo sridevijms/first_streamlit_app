3b81c7c48db716c42b00584943b12b891fd83950
Sridevi <sridevijms@gmail.com>
2022-11-05 01:29:36 +0530

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,10 +53,10 @@ except URLError as e:
     streamlit.error()
 
 
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+# streamlit.stop()
+#
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
